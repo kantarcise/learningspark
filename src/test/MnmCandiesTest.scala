551842da5894c5eeddba46f7ab813e25fa213b72
kantarcise <sezaiburakkantarci@gmail.com>
2024-07-05 21:46:31 +0300

    added test case for different data types

--- src/test/MnmCandiesTest.scala
+++ src/test/MnmCandiesTest.scala
@@ -94,27 +94,78 @@ class MnmCandiesTest extends AnyFunSuite {
                   ("NY", "Red", 1))
 
     // We can Convert to List
+
+    //    List:
+    //
+    //    Immutable.
+    //    Maintains insertion order.
+    //    Suitable for pattern matching and recursive algorithms.
+
     val listResult: List[(String, String, Int)] = seq.toList
 
     // We can Convert to Array
+
+    //     Array:
+    //
+    //    Mutable.
+    //    Fixed size.
+    //    Suitable for scenarios where you need fast random access.
+
     val arrayResult: Array[(String, String, Int)] = seq.toArray
 
     // We can Convert to Vector
+
+    //     Vector:
+    //
+    //    Immutable.
+    //    Provides fast random access and updates.
+    //    Suitable for scenarios where frequent modifications are needed.
+
     val vectorResult: Vector[(String, String, Int)] = seq.toVector
 
     // We can Convert to Set
+
+    //    Set:
+    //
+    //    Immutable by default.
+    //    Does not maintain order.
+    //    Ensures unique elements.
+
     val setResult: Set[(String, String, Int)] = seq.toSet
 
     // We can Convert to Map
+
+    //    Map:
+    //
+    //    Immutable by default.
+    //    Key-value pairs.
+    //    Keys must be unique.
+    //    Suitable when you need to associate keys with values.
+
     val mapResult: Map[(String, String), Int] = seq.map { case (state, color, count) => ((state, color), count) }.toMap
 
     // We can Convert to Mutable ListBuffer
+
+    //    Mutable ListBuffer:
+    //
+    //    Mutable.
+    //    Maintains insertion order.
+    //    Suitable for building lists efficiently.
+
     import scala.collection.mutable.ListBuffer
     val listBufferResult: ListBuffer[(String, String, Int)] = ListBuffer(seq: _*)
 
     // We can Convert to Mutable ArrayBuffer
+
+    //     Mutable ArrayBuffer:
+    //
+    //    Mutable.
+    //    Similar to an array but resizable.
+    //    Suitable for scenarios where the size of the array can change dynamically.
     import scala.collection.mutable.ArrayBuffer
     val arrayBufferResult: ArrayBuffer[(String, String, Int)] = ArrayBuffer(seq: _*)
+
+    // we can use all of these in different tests!
   }
 
 }
