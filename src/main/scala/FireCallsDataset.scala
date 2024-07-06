4c570784008ce20371b7338b03350aa12098d9d8
kantarcise <sezaiburakkantarci@gmail.com>
2024-07-06 08:12:47 +0300

    Improvement, noi longer uses inferSchema

--- src/main/scala/FireCallsDataset.scala
+++ src/main/scala/FireCallsDataset.scala
@@ -2,6 +2,7 @@ package learningSpark
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
 
 // Same example, with Datasets
 object FireCallsDataset {
@@ -26,11 +27,49 @@ object FireCallsDataset {
         s"$projectDir/data/sf-fire-calls.csv"
       }
 
+      // Define schema - if we dont, we will get an error
+      // Because when Spark loads data from CSV
+      // file, and it infers all the fields as StringType by default.
+      // In other words, when without a defined schema or inferSchema,
+      // Spark treats all columns as StringType by default.
+      val fireSchema = StructType(
+        Array(
+          StructField("CallNumber", IntegerType, nullable = true),
+          StructField("UnitID", StringType, nullable = true),
+          StructField("IncidentNumber", IntegerType, nullable = true),
+          StructField("CallType", StringType, nullable = true),
+          StructField("CallDate", StringType, nullable = true),
+          StructField("WatchDate", StringType, nullable = true),
+          StructField("CallFinalDisposition", StringType, nullable = true),
+          StructField("AvailableDtTm", StringType, nullable = true),
+          StructField("Address", StringType, nullable = true),
+          StructField("City", StringType, nullable = true),
+          StructField("Zipcode", IntegerType, nullable = true),
+          StructField("Battalion", StringType, nullable = true),
+          StructField("StationArea", StringType, nullable = true),
+          StructField("Box", StringType, nullable = true),
+          StructField("OriginalPriority", StringType, nullable = true),
+          StructField("Priority", StringType, nullable = true),
+          StructField("FinalPriority", IntegerType, nullable = true),
+          StructField("ALSUnit", BooleanType, nullable = true),
+          StructField("CallTypeGroup", StringType, nullable = true),
+          StructField("NumAlarms", IntegerType, nullable = true),
+          StructField("UnitType", StringType, nullable = true),
+          StructField("UnitSequenceInCallDispatch", IntegerType, nullable = true),
+          StructField("FirePreventionDistrict", StringType, nullable = true),
+          StructField("SupervisorDistrict", StringType, nullable = true),
+          StructField("Neighborhood", StringType, nullable = true),
+          StructField("Location", StringType, nullable = true),
+          StructField("RowID", StringType, nullable = true),
+          StructField("Delay", DoubleType, nullable = true)
+        )
+      )
+
       // Read the CSV file into a Dataset
       val fireCallsDS = spark.read
         .format("csv")
         .option("header", value = true)
-        .option("inferSchema", value = true)
+        .schema(fireSchema)
         .load(fireCallsPath)
         .as[FireCallInstance]
 
