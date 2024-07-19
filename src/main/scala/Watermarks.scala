7e16b198e1ac45960dcf840335db7c702662ec08
kantarcise <sezaiburakkantarci@gmail.com>
2024-07-19 22:48:45 +0300

    Watermarks to prevent unbounded state 🐤

---
+++ src/main/scala/Watermarks.scala
@@ -0,0 +1,138 @@
+package learningSpark
+
+import org.apache.spark.sql._
+import org.apache.spark.sql.execution.streaming.MemoryStream
+import org.apache.spark.sql.functions.window
+import org.apache.spark.sql.streaming.StreamingQuery
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration.FiniteDuration
+
+import java.sql.Timestamp
+
+/** With watermarks, we will know the point at which no more data
+ * will arrive for a given group, and the engine can automatically
+ * finalize the aggregates of certain groups and drop them  from the state.
+ *
+ * This limits the total amount of state that the engine has to
+ * maintain to compute the results of the query :)
+ */
+object Watermarks {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder()
+      .appName("Watermarks")
+      .master("local[*]")
+      .getOrCreate()
+
+    import spark.implicits._
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    // with the help of listener, we can see information like
+    // numRowsDroppedByWatermark
+    spark.streams.addListener(new CustomListener)
+
+    // Initialize MemoryStream
+    val sensorMemoryStream = new
+        MemoryStream[SimpleSensorWithTimestamp](1, spark.sqlContext)
+
+    // Add sample data every 3 seconds - one by one!
+    val addDataFuture = addDataPeriodicallyToMemoryStream(
+      sensorMemoryStream, 3.seconds)
+
+    val sensorStreamWithTimestamp: Dataset[SimpleSensorWithTimestamp] = sensorMemoryStream
+      .toDS()
+
+    // Perform aggregation, tumblingWindow
+    // Our window will start at {2022-01-02 10:00:00}
+    // and it will end in 2022-01-02 10:04:00.
+    val aggregatedStreamTumbling: DataFrame = sensorStreamWithTimestamp
+      // two-minute windows as a dynamically computed grouping column.
+      .withWatermark("eventTime", "2 minutes")
+      .groupBy(window($"eventTime", "2 minutes"), $"device_id")
+      .count()
+
+    // Perform aggregation, slidingWindow
+    // Our window will be {2022-01-02 09:59:00, 2022-01-02 10:01:00}
+    // to {2022-01-02 10:02:00, 2022-01-02 10:04:00} - 1 minute sliding
+    // some events will be in multiple groups!
+    val aggregatedStreamSliding: DataFrame = sensorStreamWithTimestamp
+      .withWatermark("eventTime", "2 minutes")
+      .groupBy(window(
+        timeColumn =  $"eventTime",
+        windowDuration =  "2 minutes",
+        slideDuration =  "1 minute"), $"device_id")
+       .count()
+
+    // In the book, it says this about the `update` mode:
+    //      This is the most useful and efficient mode to
+    //      run queries with streaming aggregations.
+    // this query will drop 8 rows, because every instance is
+    // in a single window
+    val query = aggregatedStreamTumbling
+      .writeStream
+      // TODO: in complete mode, watermarks doesn't work. Why?
+      //  The answer in the book, end of page 245!
+      .outputMode("update")
+      .format("console")
+      // we can see the print out in full
+      .option("truncate" , false)
+      .start()
+
+    // this query will drop 16 rows, because of sliding window
+    val querySecond: StreamingQuery = aggregatedStreamSliding
+      .writeStream
+      .outputMode("update")
+      .format("console")
+      .option("truncate", false)
+      .start()
+
+    // in the console, we will see
+    // the numRowsDroppedByWatermark :)
+    query.awaitTermination()
+    querySecond.awaitTermination()
+
+    // Wait for the data adding to finish (it won't, but in a real
+    // use case you might want to manage this better)
+    Await.result(addDataFuture, Duration.Inf)
+  }
+
+  /**
+   Add generated data on time interval, to an existing MemoryStream.
+
+   This simulates a real streaming scenario where data arrives continuously.
+
+   This time, let's use timestamps directly. :)
+   */
+  def addDataPeriodicallyToMemoryStream(memoryStream: MemoryStream[SimpleSensorWithTimestamp],
+                                        interval: FiniteDuration): Future[Unit] = Future {
+
+    // Random temperatures - in between 20 and 80 celsius
+    val r = new scala.util.Random
+
+    // every sensor in every other minute, with some late data!
+    val testData: Seq[SimpleSensorWithTimestamp] = Seq(
+      SimpleSensorWithTimestamp(1, 20 + r.nextInt(61), Timestamp.valueOf("2022-01-02 10:00:00")),
+      SimpleSensorWithTimestamp(2, 20 + r.nextInt(61), Timestamp.valueOf("2022-01-02 10:01:00")),
+      SimpleSensorWithTimestamp(3, 20 + r.nextInt(61), Timestamp.valueOf("2022-01-02 10:02:00")),
+      // a late data!
+      SimpleSensorWithTimestamp(4, 20 + r.nextInt(61), Timestamp.valueOf("2022-01-01 09:04:00")),
+      SimpleSensorWithTimestamp(3, 20 + r.nextInt(61), Timestamp.valueOf("2022-01-01 09:05:00")),
+      SimpleSensorWithTimestamp(4, 20 + r.nextInt(61), Timestamp.valueOf("2022-01-01 09:06:00")),
+      SimpleSensorWithTimestamp(4, 20 + r.nextInt(61), Timestamp.valueOf("2022-01-01 09:07:00")),
+      SimpleSensorWithTimestamp(4, 20 + r.nextInt(61), Timestamp.valueOf("2022-01-01 09:08:00")),
+      SimpleSensorWithTimestamp(420, 20 + r.nextInt(61), Timestamp.valueOf("2022-01-01 09:09:00")),
+      SimpleSensorWithTimestamp(4, 20 + r.nextInt(61), Timestamp.valueOf("2022-01-01 09:10:00")),
+      SimpleSensorWithTimestamp(4, 20 + r.nextInt(61), Timestamp.valueOf("2022-01-01 09:11:00")),
+    )
+
+    testData.foreach { record =>
+      memoryStream.addData(record)
+      Thread.sleep(interval.toMillis)
+    }
+  }
+}
---
+++ src/main/scala/WatermarksWithSocket.scala
@@ -0,0 +1,75 @@
+package learningSpark
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions._
+
+/**
+ * This example was taken from
+ * https://github.com/NeerajBhadani/spark-streaming
+ * It is for to demonstrate how Watermarks work
+ * with a socket stream!
+ *
+ * To see the effect of watermarking, before you run the Spark Application
+ * run `nc -lk 9999` in the console and type:
+ *
+ * 2021-01-01 10:06:00#2
+ * 2021-01-01 10:13:00#5
+ * 2020-12-31 10:06:00#1 - (this will be discarded)
+ * 2021-01-01 10:08:00#8
+ * 2021-01-01 10:00:01#7
+ * 2021-01-01 9:59:00#3 - (this will be discarded)
+ */
+object WatermarksWithSocket {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder()
+      .master("local")
+      .appName("WatermarksWithSocket")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    // Define host and port number to Listen.
+    val host = "127.0.0.1"
+    val port = "9999"
+
+    // Make Streaming DataFrame by reading data from socket.
+    val initDF: DataFrame = spark
+      .readStream
+      .format("socket")
+      .option("host", host)
+      .option("port", port)
+      .load()
+
+    // Make DataFrame  with event_timestamp and val column
+    val eventDF: DataFrame = initDF
+      // split the data based on #
+      .select(split(col("value"), "#").as("data"))
+      // make a timestamp column
+      .withColumn("event_timestamp", element_at(col("data"),1).cast("timestamp"))
+      .withColumn("val", element_at(col("data"),2).cast("int"))
+      .drop("data")
+
+    // Without Watermarking
+    // val resultDF = eventDF
+    //   .groupBy(window(col("event_timestamp"), "5 minute"))
+    //   .agg(sum("val").as("sum"))
+
+    // with 10 min watermark
+    val resultDF: DataFrame = eventDF
+      .withWatermark("event_timestamp", "10 minutes")
+      .groupBy(window(col("event_timestamp"), "5 minute"))
+      .agg(sum("val").as("sum"))
+
+    // Display DataFrame on console.
+    resultDF
+      .writeStream
+      .outputMode("update")
+      .option("truncate", false)
+      .option("numRows", 10)
+      .format("console")
+      .start()
+      .awaitTermination()
+  }
+}
