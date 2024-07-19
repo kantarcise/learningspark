11a53db7889a29972d422a723721108bd3340fa2
kantarcise <sezaiburakkantarci@gmail.com>
2024-07-19 22:09:36 +0300

    we can select what we are monitoring! 🥳

--- src/main/scala/CustomListener.scala
+++ src/main/scala/CustomListener.scala
@@ -3,18 +3,28 @@ package learningSpark
 import org.apache.spark.sql.streaming.StreamingQueryListener.{QueryProgressEvent, QueryStartedEvent, QueryTerminatedEvent}
 import org.apache.spark.sql.streaming._
 
-// we can use this class to setup a custom
-// listener for our SparkStreaming Queries!
+/** We can use this class to setup a custom
+ * listener for our SparkStreaming Queries!
+ */
 class CustomListener extends StreamingQueryListener {
   override def onQueryStarted(event: QueryStartedEvent): Unit = {
     println("Query started: " + event.id)
+    println("Query: " + event.name)
+
   }
 
   override def onQueryTerminated(event: QueryTerminatedEvent): Unit = {
     println("Query terminated: " + event.id)
   }
 
+  /**
+    Adjust this progress method based on your needs!
+   */
   override def onQueryProgress(event: QueryProgressEvent): Unit = {
-    println("Query made progress: " + event.progress)
+    // println("Query made progress: " + event.progress)
+    // we can see what are we interested in,
+    // let's only see numRowsDroppedByWatermark
+    println("Query made progress: numRowsDroppedByWatermark: " +
+      event.progress.stateOperators(0).numRowsDroppedByWatermark)
   }
 }
