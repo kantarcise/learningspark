3a8e1d75cfb587a25a589092e149becac5eb0a4d
kantarcise <sezaiburakkantarci@gmail.com>
2024-08-03 11:37:38 +0300

    Add more comments 😌

--- src/main/scala/Aggregates.scala
+++ src/main/scala/Aggregates.scala
@@ -1,6 +1,6 @@
 package learningSpark
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Dataset, SparkSession}
 import org.apache.spark.sql.functions._
 
 object Aggregates {
@@ -18,16 +18,17 @@ object Aggregates {
       .getOrCreate()
 
     // verbose = False
-    // Just so that we can get the result
-    // without all the console logs
+    // Just so that we can see the result of our application
+    // without all the INFO console logs
     spark.sparkContext.setLogLevel("WARN")
 
-    // what is this?
+    // Wait, what is this?
     // https://stackoverflow.com/q/50878224
     import spark.implicits._
 
     // Make a DataFrame of names and ages
-    val peopleDF = spark.createDataFrame(
+    val peopleDF = spark
+      .createDataFrame(
         Seq(
           ("Brooke", 20),
           ("Brooke", 25),
@@ -38,20 +39,22 @@ object Aggregates {
       )
       .toDF("name", "age")
 
-    // there is also, toDS
+    // there is also, toDS() method available for us.
     // Create a Dataset from a sequence of case class instances
+    // if this is confusing, do not worry!
     val people = Seq(
       SimplePerson("Alice", 29),
       SimplePerson("Bob", 31))
 
     // here is our Dataset
-    val peopleDS = people
+    // We use colons for Type Annotations!
+    val peopleDS: Dataset[SimplePerson] = people
       .toDS()
 
     // Show the Dataset
     peopleDS.show()
 
-    // Create a DataFrame from a sequence of tuples
+    // Make a DataFrame from a sequence of tuples
     val randomDF = Seq((1, "foo"), (2, "bar"))
       .toDF("id", "value")
 
@@ -81,7 +84,7 @@ object Aggregates {
      +------+--------+
      */
 
-    // Just like max, we can use other functions.
+    // Just like max, we can use other functions!
     // Group the same names together,
     // aggregate their ages, and compute an average
     val avgAgeDF = peopleDF
