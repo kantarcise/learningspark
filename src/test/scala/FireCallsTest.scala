05d6f84708accbca290859ff06330067cbcdc6fa
kantarcise <sezaiburakkantarci@gmail.com>
2024-08-04 09:55:27 +0300

    Updating Case classes and tests 😎

--- src/test/scala/FireCallsTest.scala
+++ src/test/scala/FireCallsTest.scala
@@ -15,11 +15,11 @@ class FireCallsTest extends AnyFunSuite {
   import spark.implicits._
 
   test("Timestamp conversion and filtering") {
-    
+
     val testData = Seq(
-        FireCallInstance(Some(1), Some("M1"), Some(1001), Some("Medical Incident"), Some("01/01/2021"), Some("01/01/2021"), Some("Other"), Some("01/01/2021 01:01:01 AM"), Some("123 Main St"), Some("San Francisco"), Some(94118), Some("B01"), Some("01"), Some("123"), Some("Low"), Some("High"), Some(1), Some(true), Some("Medical"), Some(1), Some("Engine"), Some(1), Some("FPD01"), Some("SD01"), Some("N01"), Some("POINT (37.7749 -122.4194)"), Some("1"), Some(1.2f)),
-        FireCallInstance(Some(2), Some("M2"), Some(1002), Some("Fire Incident"), Some("02/01/2021"), Some("02/01/2021"), Some("Other"), Some("02/01/2021 02:02:02 AM"), Some("456 Main St"), Some("San Francisco"), Some(94118), Some("B02"), Some("02"), Some("456"), Some("Medium"), Some("Medium"), Some(2), Some(false), Some("Fire"), Some(2), Some("Truck"), Some(2), Some("FPD02"), Some("SD02"), Some("N02"), Some("POINT (37.7749 -122.4194)"), Some("2"), Some(2.3f)),
-        FireCallInstance(Some(3), Some("M3"), Some(1003), Some("False Alarm"), Some("03/01/2021"), Some("03/01/2021"), Some("Other"), Some("03/01/2021 03:03:03 AM"), Some("789 Main St"), Some("San Francisco"), Some(94118), Some("B03"), Some("03"), Some("789"), Some("High"), Some("Low"), Some(3), Some(true), Some("Alarm"), Some(3), Some("Ambulance"), Some(3), Some("FPD03"), Some("SD03"), Some("N03"), Some("POINT (37.7749 -122.4194)"), Some("3"), Some(3.4f))
+        FireCallInstance(Some(1), Some("M1"), Some(1001), Some("Medical Incident"), Some("01/01/2021"), Some("01/01/2021"), Some("Other"), Some("01/01/2021 01:01:01 AM"), Some("123 Main St"), Some("San Francisco"), Some(94118), Some("B01"), Some("01"), Some("123"), Some("Low"), Some("High"), Some(1), Some(true), Some("Medical"), Some(1), Some("Engine"), Some(1), Some("FPD01"), Some("SD01"), Some("N01"), Some("POINT (37.7749 -122.4194)"), Some("1"), Some(1.2)),
+        FireCallInstance(Some(2), Some("M2"), Some(1002), Some("Fire Incident"), Some("02/01/2021"), Some("02/01/2021"), Some("Other"), Some("02/01/2021 02:02:02 AM"), Some("456 Main St"), Some("San Francisco"), Some(94118), Some("B02"), Some("02"), Some("456"), Some("Medium"), Some("Medium"), Some(2), Some(false), Some("Fire"), Some(2), Some("Truck"), Some(2), Some("FPD02"), Some("SD02"), Some("N02"), Some("POINT (37.7749 -122.4194)"), Some("2"), Some(2.3)),
+        FireCallInstance(Some(3), Some("M3"), Some(1003), Some("False Alarm"), Some("03/01/2021"), Some("03/01/2021"), Some("Other"), Some("03/01/2021 03:03:03 AM"), Some("789 Main St"), Some("San Francisco"), Some(94118), Some("B03"), Some("03"), Some("789"), Some("High"), Some("Low"), Some(3), Some(true), Some("Alarm"), Some(3), Some("Ambulance"), Some(3), Some("FPD03"), Some("SD03"), Some("N03"), Some("POINT (37.7749 -122.4194)"), Some("3"), Some(3.4))
       ).toDS()
 
     val updatedFireCallsDS = testData
