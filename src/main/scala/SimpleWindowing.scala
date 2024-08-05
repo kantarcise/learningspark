d1dea6b7435509e7b1b73c8048128306ee8c939c
kantarcise <sezaiburakkantarci@gmail.com>
2024-08-05 21:55:47 +0300

    Window and Window Functions ⛺

--- src/main/scala/SimpleWindowing.scala
+++ src/main/scala/SimpleWindowing.scala
@@ -4,19 +4,21 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
 
+/**
+ * Let's understand how Windows work in Spark!
+ */
 object SimpleWindowing {
 
   def main(args: Array[String]): Unit = {
 
-    val spark = SparkSession.builder
+    val spark = SparkSession
+      .builder
       .appName("Window Function Example")
       .master("local[*]")
       .getOrCreate()
 
-    // $ usage
     import spark.implicits._
 
-    // verbose = False
     spark.sparkContext.setLogLevel("ERROR")
 
     // Sample sales data
@@ -38,7 +40,7 @@ object SimpleWindowing {
       .groupBy("region", "salesperson")
       .agg(sum("amount").as("total_sales"))
 
-    println("groupBy region, salesperson\n")
+    println("groupBy region, salesperson, with total_sales\n")
     totalSales.show()
 
     // Define window specification
@@ -63,19 +65,19 @@ object SimpleWindowing {
     println("Ranked Sales with dense_rank\n")
     denseRankedSales.show()
 
-    // here is a more general comparison of window functions
+    println("Here is a more general comparison of window functions\n")
 
     val randomDF = spark.createDataFrame(Seq(
       ("a", 10), ("a", 20), ("a", 30),
       ("a", 40), ("a", 40), ("a", 40), ("a", 40),
       ("a", 50), ("a", 50), ("a", 60)
-    )).toDF("part_col", "order_col")
+    )).toDF("part_name", "order_count")
 
     val window = Window
       // all the same parts = a
-      .partitionBy("part_col")
-      // ordered by order_col
-      .orderBy("order_col")
+      .partitionBy("part_name")
+      // ordered by order_count
+      .orderBy(desc("order_count"))
 
     val dfWithRanks = randomDF
       .withColumn("rank", rank().over(window))
