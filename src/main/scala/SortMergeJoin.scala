38940ed57fd937bc3a8696768b939d002b618aac
kantarcise <sezaiburakkantarci@gmail.com>
2024-08-08 19:18:45 +0300

    Less friction, more configs 🍒

--- src/main/scala/SortMergeJoin.scala
+++ src/main/scala/SortMergeJoin.scala
@@ -3,24 +3,32 @@ package learningSpark
 import org.apache.spark.sql.SparkSession
 import scala.util.Random
 
-// heavily inspired from the books example
+/** This application is heavily inspired
+ * from the books example.
+ *
+ * You can check out Page 190.
+ */
 object SortMergeJoin {
 
-  // curried function to benchmark any code or function
+  /** To benchmark any code or function
+   *
+   * @param name: Name of method
+   * @param f: method
+   */
   def benchmark(name: String)(f: => Unit) {
     val startTime = System.nanoTime
     f
     val endTime = System.nanoTime
-    println(s"Time taken in $name: " + (endTime - startTime).toDouble / 1000000000 + " seconds")
+    println(s"Time taken in $name: " +
+      (endTime - startTime).toDouble / 1000000000 +
+      " seconds")
   }
 
-  // main class setting the configs
-  def main (args: Array[String] ) {
+  def main(args: Array[String] ): Unit = {
 
     val spark = SparkSession
       .builder
-      .appName("SortMergeJoin")
-      // this alone is not enough
+      .appName("Learning More About Sort Merge Join")
       .config("spark.sql.join.preferSortMergeJoin", true)
       // Specifying a value of -1 in spark.sql.autoBroadcastJoinThreshold
       // will cause Spark to always resort to a shuffle sort merge join
@@ -28,11 +36,10 @@ object SortMergeJoin {
       // The default value is 200, for a 12 core machine
       .config("spark.sql.shuffle.partitions", 24)
       .master(master = "local[*]")
-      .getOrCreate ()
+      .getOrCreate()
 
     import spark.implicits._
 
-    // Verbose = false
     spark.sparkContext.setLogLevel("ERROR")
 
     var states = scala.collection.mutable.Map[Int, String]()
@@ -40,14 +47,17 @@ object SortMergeJoin {
     // seed
     val rnd = new Random(42)
 
-    // this should be 16
+    // let's check the configuration.
     println(spark.conf.get("spark.sql.shuffle.partitions"))
+    println(spark.conf.get("spark.sql.join.preferSortMergeJoin"))
 
     // initialize states and items purchased
     // they are MutableMaps with int keys and string values.
     // When making Dataframes, we will choose randomly from them
-    states += (0 -> "AZ", 1 -> "CO", 2-> "CA", 3-> "TX", 4 -> "NY", 5-> "MI")
-    items += (0 -> "SKU-0", 1 -> "SKU-1", 2-> "SKU-2", 3-> "SKU-3", 4 -> "SKU-4", 5-> "SKU-5")
+    states += (0 -> "AZ", 1 -> "CO", 2-> "CA",
+      3-> "TX", 4 -> "NY", 5-> "MI")
+    items += (0 -> "SKU-0", 1 -> "SKU-1", 2-> "SKU-2",
+      3-> "SKU-3", 4 -> "SKU-4", 5-> "SKU-5")
 
     // make the dataframes
     val usersDF = (0 to 100000)
@@ -72,6 +82,7 @@ object SortMergeJoin {
           "amount", "state", "items")
 
     // lets see how they look
+    println("Here are our users and orders Dataframes\n:")
     usersDF.show(10)
     ordersDF.show(10)
 
@@ -81,13 +92,14 @@ object SortMergeJoin {
     println("Result of the Sort Merge Join\n")
     usersOrdersDF.show(truncate = false)
     usersOrdersDF.cache()
+
+    println("Let's double check that we are using SortMergeJoin\n:")
     usersOrdersDF.explain()
     // usersOrdersDF.explain("formated")
 
     // uncomment to view the SparkUI
     // if you open the UI, IT shows three stages for the
     // entire job: the Exchange and Sort operations happen in the final stage
-    Thread.sleep(1000 * 60 * 7)
+    Thread.sleep(1000 * 60 * 3)
   }
-
 }
