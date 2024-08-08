df4376b15fbc5cf4557b59e194a7cfddf035d0fd
kantarcise <sezaiburakkantarci@gmail.com>
2024-08-08 20:30:47 +0300

    Needs more work, but first version 😌

--- src/main/scala/MapAndMapPartitions.scala
+++ src/main/scala/MapAndMapPartitions.scala
@@ -4,43 +4,51 @@ import org.apache.spark.sql.SparkSession
 import scala.math.sqrt
 import java.io.FileWriter
 
-// this is taken from the books github page.
-// It looks like it has been removed from the book itself.
+/**
+ * This application is taken from the books
+ * github page.
+ *
+ * It looks like it has been removed from
+ * the book itself.
+ */
 object MapAndMapPartitions {
 
-  // simulate a connection to a FileSystem
+  /** Simulate a connection to a FileSystem
+   *
+   * @param f: A string of filename for FileWriter!
+   * @return
+   */
   def getConnection (f: String): FileWriter = {
     new FileWriter(f, true)
   }
 
-  /*
-    This method computes the square root of a given
-    value, writes the result to a file, and returns
-    the square root.
-
-    This method is intended to be used with
-    the map transformation.
+  /**
+   * This method computes the square root of a given
+   * value, writes the result to a file, and returns
+   * the square root.
+   *
+   * This method is intended to be used with
+   * the map transformation.
    */
-  def func(v: Long): Double = {
+  def funcForSquareRoot(v: Long): Double = {
     // make a connection to DB
     val conn: FileWriter = getConnection("/tmp/sqrt.txt")
     val sr: Double = sqrt(v)
     // write value out to DB
-    conn.write(sr.toString())
+    conn.write(sr.toString)
     conn.write(System.lineSeparator())
     conn.close()
     sr
   }
 
-  /*
-    This method is similar to func, but it reuses the
-    provided FileWriter connection.
-
-    This is intended to be used with the mapPartitions
-    transformation to avoid opening and closing the
-    connection repeatedly within each partition.
-
-    Intended to be used with mapPartition
+  /**
+   * This method is similar to func, but it reuses the
+   * provided FileWriter connection.
+   *
+   * This is intended to be used with the mapPartitions
+   * transformation to avoid opening and closing the
+   * connection repeatedly within each partition.
+   * Intended to be used with mapPartition
    */
   def funcMapPartions(conn: FileWriter, v: Long): Double = {
     val sr: Double = sqrt(v)
@@ -61,7 +69,7 @@ object MapAndMapPartitions {
       " seconds")
   }
 
-  def main (args: Array[String] ) {
+  def main(args: Array[String] ): Unit = {
 
     val spark: SparkSession = SparkSession
       .builder
@@ -71,27 +79,28 @@ object MapAndMapPartitions {
 
     import spark.implicits._
 
-    // verbose = False
     spark.sparkContext.setLogLevel("ERROR")
 
     val df = spark
       .range(1 * 10000000)
       .toDF("id")
       .withColumn("square", $"id" * $"id")
-      .repartition(16)
+      //
+      .repartition(12)
 
-    println("\n Here is a simple Dataframe\n")
+    println("\n Here is a simple Dataframe:\n")
     df.show(5)
 
-    println("\n Benchmarking Map Function")
+    println("\nBenchmarking Map Function:\n")
+
     // Benchmark Map function
     benchmark("map function") {
       df
-        .map(r => (func(r.getLong(1))))
+        .map(r => (funcForSquareRoot(r.getLong(1))))
         .show(10)
     }
 
-    println("\n Benchmarking Map Partition Function")
+    println("\nBenchmarking Map Partition Function:")
     // Benchmark MapPartition function
     benchmark("mapPartition function") {
       df
@@ -110,10 +119,7 @@ object MapAndMapPartitions {
         .toDF()
         .show(10)
     }
-
     println("\nThe mapPartitions approach is expected to be more " +
       "efficient due to fewer connections being opened and closed.")
-
   }
-
 }
