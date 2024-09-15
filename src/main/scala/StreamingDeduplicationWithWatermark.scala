e2be66951a1629dd3281ac3c21030d66ceafffa8
kantarcise <sezaiburakkantarci@gmail.com>
2024-09-15 08:05:37 +0300

    Better docs, watermark 📏

--- src/main/scala/StreamingDeduplicationWithWatermark.scala
+++ src/main/scala/StreamingDeduplicationWithWatermark.scala
@@ -1,7 +1,6 @@
 package learningSpark
 
 import org.apache.spark.sql.execution.streaming.MemoryStream
-import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 import java.sql.Timestamp
@@ -20,8 +19,12 @@ import scala.concurrent.duration.FiniteDuration
 object StreamingDeduplicationWithWatermark {
 
   /**
-   * Let's say we have some events that should be happening uniquely
-   * */
+   * Represents an event with a unique identifier, timestamp, and associated data.
+   *
+   * @param guid      Unique identifier for the event.
+   * @param eventTime Timestamp of when the event occurred.
+   * @param data      Additional data related to the event.
+   */
   case class Event(guid: String, eventTime: Timestamp, data: String)
 
   def main(args: Array[String]): Unit = {
@@ -58,7 +61,7 @@ object StreamingDeduplicationWithWatermark {
       .queryName("Deduplication with Watermark to Console")
       .outputMode("append")
       .format("console")
-      .option("truncate", false)
+      .option("truncate", value = false)
       .start()
 
     queryWithWatermark.awaitTermination()
@@ -76,20 +79,31 @@ object StreamingDeduplicationWithWatermark {
                                         interval: FiniteDuration): Future[Unit] = Future {
 
     val sampleData: Seq[Event] = Seq(
-      Event("862207ec-14d1-4671-9e61-56820f1a8b14", Timestamp.valueOf("2023-06-09 12:00:00"), "data1"),
-      Event("8562e978-54fd-4fc5-b70c-2b8f7e1346b5", Timestamp.valueOf("2023-06-09 12:11:00"), "data2"),
-      Event("0f1d9d2f-9044-45fb-a744-dd7312a0c4f1", Timestamp.valueOf("2023-06-09 12:22:00"), "data3"),
+      Event("862207ec-14d1-4671-9e61-56820f1a8b14",
+        Timestamp.valueOf("2023-06-09 12:00:00"), "data1"),
+      Event("8562e978-54fd-4fc5-b70c-2b8f7e1346b5",
+        Timestamp.valueOf("2023-06-09 12:11:00"), "data2"),
+      Event("0f1d9d2f-9044-45fb-a744-dd7312a0c4f1",
+        Timestamp.valueOf("2023-06-09 12:22:00"), "data3"),
       // these are duplicates but not within watermark
-      Event("862207ec-14d1-4671-9e61-56820f1a8b14", Timestamp.valueOf("2023-06-09 09:21:00"), "data1_duplicate"),
-      Event("8562e978-54fd-4fc5-b70c-2b8f7e1346b5", Timestamp.valueOf("2023-06-09 10:20:00"), "data2_duplicate"),
-      Event("aaa5e41e-8127-4fc2-a93d-8c15f3aa3e42", Timestamp.valueOf("2023-06-09 12:55:00"), "data4"),
-      Event("a4ad4480-d39b-495f-8940-31d2d148b29c", Timestamp.valueOf("2023-06-09 13:16:00"), "data5"),
+      Event("862207ec-14d1-4671-9e61-56820f1a8b14",
+        Timestamp.valueOf("2023-06-09 09:21:00"), "data1_duplicate"),
+      Event("8562e978-54fd-4fc5-b70c-2b8f7e1346b5",
+        Timestamp.valueOf("2023-06-09 10:20:00"), "data2_duplicate"),
+      Event("aaa5e41e-8127-4fc2-a93d-8c15f3aa3e42",
+        Timestamp.valueOf("2023-06-09 12:55:00"), "data4"),
+      Event("a4ad4480-d39b-495f-8940-31d2d148b29c",
+        Timestamp.valueOf("2023-06-09 13:16:00"), "data5"),
       // this is duplicate but not within watermark
-      Event("862207ec-14d1-4671-9e61-56820f1a8b14", Timestamp.valueOf("2023-06-09 12:00:00"), "data1_duplicate2"),
+      Event("862207ec-14d1-4671-9e61-56820f1a8b14",
+        Timestamp.valueOf("2023-06-09 12:00:00"), "data1_duplicate2"),
       // this is a new Event and sets a new time max!
       // so even though it is a duplicate, it will not be dropped
-      Event("8562e978-54fd-4fc5-b70c-2b8f7e1346b5", Timestamp.valueOf("2023-06-09 13:38:00"), "data2_duplicate2"),
-      Event("54b5d173-8c54-4a34-87f4-d8bf7c312b08", Timestamp.valueOf("2023-06-09 13:49:00"), "data6")
+      Event("8562e978-54fd-4fc5-b70c-2b8f7e1346b5",
+        Timestamp.valueOf("2023-06-09 13:38:00"),
+        "data2_duplicate2_but_new_max_time"),
+      Event("54b5d173-8c54-4a34-87f4-d8bf7c312b08",
+        Timestamp.valueOf("2023-06-09 13:49:00"), "data6")
     )
 
     sampleData.foreach { record =>
