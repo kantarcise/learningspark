efe149bd6b44084c80276efd660c5c57ec38195e
kantarcise <sezaiburakkantarci@gmail.com>
2024-09-16 20:25:10 +0300

    Leverage implicit parameters, mapGroupsWithState 🎋

--- src/main/scala/UnmanagedStatefulOperations.scala
+++ src/main/scala/UnmanagedStatefulOperations.scala
@@ -30,90 +30,105 @@ object UnmanagedStatefulOperations {
       active = action.action match {
         case "move" => true
         case "idle" => false
-        case _ => active
+        case _      => active
       }
     }
   }
 
   def main(args: Array[String]): Unit = {
-
-    val spark = SparkSession
-      .builder
-      .appName("Unmanaged State Processing Time Timeouts")
-      .master("local[*]")
-      .getOrCreate()
-
-    import spark.implicits._
+    // let's try to decompose the code
+    implicit val spark: SparkSession = createSparkSession()
 
     spark.sparkContext.setLogLevel("WARN")
 
-    // Initialize MemoryStream
-    val userActionMemoryStream = new
-        MemoryStream[UserAction](1, spark.sqlContext)
+    val userActionMemoryStream = initializeMemoryStream()
+    val addDataFuture = addDataPeriodically(userActionMemoryStream, 1.second)
 
-    // Add sample data every second - one by one
-    val addDataFuture = addDataPeriodicallyToUserActionMemoryStream(
-      userActionMemoryStream,
-      1.seconds)
+    val userActions = userActionMemoryStream.toDS()
+    val latestStatuses = computeLatestStatuses(userActions)
+    val query = startStreamingQuery(latestStatuses)
 
-    val userActions: Dataset[UserAction] = userActionMemoryStream
-      .toDS()
+    query.awaitTermination()
+    Await.result(addDataFuture, Duration.Inf)
+  }
 
-    // when we run this query, we will see these
-    // final UserStatus for each user:
-    /** +------+------+
-     *  |userId|active|
-     *  +------+------+
-     *  |     1| false|
-     *  |     3|  true|
-     *  |     2|  true|
-     */
-    val latestStatuses = userActions
-      // group by user id
-      .groupByKey(userAction => userAction.userId)
-      //.mapGroupsWithState(GroupStateTimeout.NoTimeout)(updateUserStatus _)
-      .mapGroupsWithState(updateUserStatus _)
+  def createSparkSession(): SparkSession = {
+    SparkSession.builder
+      .appName("Unmanaged Stateful Operations")
+      .master("local[*]")
+      .getOrCreate()
+  }
 
-    // Start the streaming query and print to console
-    val query = latestStatuses
+  /**
+   *  In Scala, a method can have parameters marked as implicit.
+   *  These parameters do not need to be passed explicitly
+   *  when calling the method, provided there is an implicit
+   *  value of the required type in the scope where
+   *  the method is called.
+   *
+   * The method has two parameter lists:
+   *    The first is empty (), indicating no explicit parameters.
+   *    The second (implicit spark: SparkSession) declares an implicit
+   *        parameter of type SparkSession.
+   *
+   * Benefits of Using Implicit Parameters:
+   *    Reduces Boilerplate: Eliminates the need to pass the same
+   *       parameter repeatedly to multiple methods.
+   *
+   *    Improves Readability: Simplifies method calls and makes code cleaner.
+   */
+  def initializeMemoryStream()(implicit spark: SparkSession): MemoryStream[UserAction] = {
+    import spark.implicits._
+    MemoryStream[UserAction](1, spark.sqlContext)
+  }
+
+  /**
+   * In our example
+   * when we run this method for our input we'll see these
+   * final UserStatus for each user:
+   *
+   * +------+------+
+   * |userId|active|
+   * +------+------+
+   * |     1| false|
+   * |     3|  true|
+   * |     2|  true|
+   *
+   *
+   * @param userActions: The UserAction Dataset
+   * @return
+   */
+  def computeLatestStatuses(userActions: Dataset[UserAction]): Dataset[UserStatus] = {
+    import userActions.sparkSession.implicits._
+
+    userActions
+      .groupByKey(_.userId)
+      .mapGroupsWithState(updateUserStatus)
+  }
+
+  def startStreamingQuery(latestStatuses: Dataset[UserStatus]): StreamingQuery = {
+    latestStatuses
       .writeStream
       .queryName("Latest Status of Users to Console")
       .outputMode("update")
       .format("console")
       .start()
-
-    query.awaitTermination()
-
-    // Wait for the data adding to finish (it won't, but in a real
-    // use case you might want to manage this better)
-    Await.result(addDataFuture, Duration.Inf)
   }
 
-  /**
-   * When this streaming query is started, in each
-   * micro-batch Spark will call this
-   * updateUserStatus() for each unique key in
-   * the micro-batch’s data.
-   *
-   * @param userId -  K
-   * @param newActions -  Iterator[V]
-   * @param state - GroupState[S]
-   * @return
-   */
-  def updateUserStatus(userId: String,
-                       newActions: Iterator[UserAction],
-                       state: GroupState[UserStatus]): UserStatus = {
+  def updateUserStatus(
+                        userId: String,
+                        newActions: Iterator[UserAction],
+                        state: GroupState[UserStatus]
+                      ): UserStatus = {
     // Get the current state or make a new state if none exists
     val userStatus = state
       .getOption
       .getOrElse(
         UserStatus(
-          userId, false))
-
+          userId, active = false))
     // Update state with new actions
-    newActions.foreach { action =>
-      userStatus.updateWith(action)
-    }
+    newActions.foreach(userStatus.updateWith)
+
     // Update the state
     state.update(userStatus)
     userStatus
@@ -123,17 +138,17 @@ object UnmanagedStatefulOperations {
    * Add generated data on time interval, to an existing MemoryStream.
    * This simulates a real streaming scenario where data arrives continuously.
    */
-  def addDataPeriodicallyToUserActionMemoryStream(memoryStream: MemoryStream[UserAction],
-                                                  interval: FiniteDuration): Future[Unit] = Future {
-
-    val sampleData: Seq[UserAction] = Seq(
+  def addDataPeriodically(memoryStream: MemoryStream[UserAction],
+                          interval: FiniteDuration
+                         ): Future[Unit] = Future {
+    val sampleData = Seq(
       UserAction("1", "move"),
       UserAction("2", "idle"),
       UserAction("3", "move"),
       // user 1 become idle and user 2 - 3 is moving
       UserAction("1", "idle"),
       UserAction("2", "move"),
-      UserAction("3", "move"),
+      UserAction("3", "move")
     )
 
     sampleData.foreach { record =>
