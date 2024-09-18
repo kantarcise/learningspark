91472a162a873ba14ee859b76a154e0c7dca454a
kantarcise <sezaiburakkantarci@gmail.com>
2024-09-18 22:09:49 +0300

    Event time timeouts, decomposed 😌

--- src/main/scala/UnmanagedStateEventTimeTimeout.scala
+++ src/main/scala/UnmanagedStateEventTimeTimeout.scala
@@ -2,7 +2,7 @@ package learningSpark
 
 import org.apache.spark.sql.{Dataset, SparkSession}
 import org.apache.spark.sql.execution.streaming.MemoryStream
-import org.apache.spark.sql.streaming.{GroupState, GroupStateTimeout}
+import org.apache.spark.sql.streaming.{GroupState, GroupStateTimeout, StreamingQuery}
 
 import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -34,7 +34,8 @@ object UnmanagedStateEventTimeTimeout {
   case class UserStatus(userId: String, var active: Boolean) {
     /**
      * Updates the user's active status based on the action.
-     * @param action: the UserAction to base the status on.
+     *
+     * @param action The UserAction to base the status on.
      */
     def updateWith(action: UserAction): Unit = {
       active = action.action match {
@@ -46,7 +47,8 @@ object UnmanagedStateEventTimeTimeout {
 
     /**
      * Marks the user as inactive.
-     * @return UserStatus with active set to false
+     *
+     * @return The updated UserStatus with active set to false.
      */
     def asInactive(): UserStatus = {
       this.active = false
@@ -54,52 +56,82 @@ object UnmanagedStateEventTimeTimeout {
     }
   }
 
+  /**
+   * A better main method, with decomposition.
+   *
+   * @param args: Array of Strings to be utilized if needed.
+   */
   def main(args: Array[String]): Unit = {
+    implicit val spark: SparkSession = createSparkSession()
+    spark.sparkContext.setLogLevel("WARN")
 
-    val spark = SparkSession
+    val userActionMemoryStream = initializeMemoryStream()
+    val addDataFuture = addDataPeriodically(userActionMemoryStream, 2.seconds)
+
+    val userActions = userActionMemoryStream.toDS()
+    val latestStatuses = computeLatestStatuses(userActions)
+    val query = startStreamingQuery(latestStatuses)
+
+    query.awaitTermination()
+    Await.result(addDataFuture, Duration.Inf)
+  }
+
+  /**
+   * Makes a SparkSession with the application
+   * name and master configuration.
+   *
+   * @return An implicit SparkSession instance.
+   */
+  def createSparkSession(): SparkSession = {
+    SparkSession
       .builder
       .appName("Unmanaged State Event Time Timeouts")
       .master("local[*]")
       .getOrCreate()
+  }
 
+  /**
+   * Initializes a MemoryStream for UserAction data.
+   *
+   * @param spark An implicit SparkSession.
+   * @return A MemoryStream of UserAction.
+   */
+  def initializeMemoryStream()(implicit spark: SparkSession): MemoryStream[UserAction] = {
     import spark.implicits._
+    new MemoryStream[UserAction](1, spark.sqlContext)
+  }
 
-    spark.sparkContext.setLogLevel("WARN")
+  /**
+   * Computes the latest user statuses using mapGroupsWithState with event time timeout.
+   *
+   * @param userActions The input Dataset of UserAction.
+   * @return A Dataset of UserStatus representing the latest statuses.
+   */
+  def computeLatestStatuses(userActions: Dataset[UserAction]): Dataset[UserStatus] = {
+    import userActions.sparkSession.implicits._
 
-    // Initialize MemoryStream
-    val userActionMemoryStream = new
-        MemoryStream[UserAction](1, spark.sqlContext)
-
-    // Add sample data every second - one by one
-    val addDataFuture = addDataPeriodicallyToUserActionMemoryStream(
-      userActionMemoryStream,
-      2.seconds)
-
-    val userActions: Dataset[UserAction] = userActionMemoryStream
-      .toDS()
-
-    val latestStatuses = userActions
+    userActions
       .withWatermark("eventTimestamp", "10 minutes")
-      .groupByKey(userAction => userAction.userId)
+      .groupByKey(_.userId)
       .mapGroupsWithState(
         GroupStateTimeout.EventTimeTimeout)(
-        updateUserStatusWithEventTimeTimeout _)
+        updateUserStatusWithEventTimeTimeout)
+  }
 
-    // Start the streaming query and print to console
-    val query = latestStatuses
+  /**
+   * Starts the streaming query to write the latest user statuses to the console.
+   *
+   * @param latestStatuses The Dataset of UserStatus to be written.
+   * @return A StreamingQuery object representing the active streaming query.
+   */
+  def startStreamingQuery(latestStatuses: Dataset[UserStatus]): StreamingQuery = {
+    latestStatuses
       .writeStream
       .queryName("Latest Status of Users to Console")
       .outputMode("update")
       .format("console")
       .start()
-
-    query.awaitTermination()
-
-    // Wait for the data adding to finish (it won't, but in a real
-    // use case you might want to manage this better)
-    Await.result(addDataFuture, Duration.Inf)
   }
-
 
   /**
    * Updates the user status based on new actions
@@ -116,26 +148,27 @@ object UnmanagedStateEventTimeTimeout {
    * new actions and sets the timeout timestamp
    * to the current watermark + 1 hour.
    *
-   * @param userId The unique identifier for the user.
+   * @param userId     The unique identifier for the user.
    * @param newActions An iterator of new actions for the
    *                   user in the current micro-batch.
-   * @param state The state corresponding to the user, which
-   *              includes their status and timeout information.
-   * @return The updated user status.
+   * @param state      The state corresponding to the user, which
+   *                   includes their status and timeout information.
+   * @return The updated UserStatus.
    */
   def updateUserStatusWithEventTimeTimeout(userId: String,
                                            newActions: Iterator[UserAction],
-                                           state: GroupState[UserStatus]): UserStatus = {
+                                           state: GroupState[UserStatus]
+                                          ): UserStatus = {
     if (!state.hasTimedOut) { // Was not called due to timeout
-      val userStatus = state.getOption.getOrElse(UserStatus(userId, false))
-      newActions.foreach { action => userStatus.updateWith(action) }
+      val userStatus = state.getOption.getOrElse(UserStatus(userId, active = false))
+      newActions.foreach(userStatus.updateWith)
       state.update(userStatus)
 
       // Set the timeout timestamp to the current watermark + 1 hour
       state.setTimeoutTimestamp(state.getCurrentWatermarkMs() + 3600000)
       userStatus
     } else {
-      val userStatus = state.get
+      val userStatus = state.getOption.getOrElse(UserStatus(userId, active = false))
       state.remove()
       userStatus.asInactive()
     }
@@ -153,23 +186,23 @@ object UnmanagedStateEventTimeTimeout {
    *
    * Specifically:
    * - User 2 moves initially but has no further actions, causing
-   *      it to be dropped due to inactivity.
+   *   it to be dropped due to inactivity.
    * - User 5 moves at the end, which causes previous
-   *      users to be dropped due to inactivity.
+   *   users to be dropped due to inactivity.
    *
    * @param memoryStream The MemoryStream to which
    *                     the sample data will be added.
-   * @param interval The interval between each
-   *                 data addition to the MemoryStream.
+   * @param interval     The interval between each
+   *                     data addition to the MemoryStream.
    * @return A Future[Unit] that completes once all the
    *         data has been added to the MemoryStream.
    */
-  def addDataPeriodicallyToUserActionMemoryStream(memoryStream: MemoryStream[UserAction],
-                                                  interval: FiniteDuration): Future[Unit]  = Future {
-
-    // user 1 went idle, so it will be false by activity
-    // user 2 will be dropped do to inactivity
+  def addDataPeriodically(memoryStream: MemoryStream[UserAction],
+                           interval: FiniteDuration
+                         ): Future[Unit] = Future {
     val sampleData: Seq[UserAction] = Seq(
+      // user 1 went idle, so it will be false by activity
+      // user 2 will be dropped due to inactivity
       UserAction("1", "move", Timestamp.valueOf("2023-06-09 12:00:00")),
       UserAction("2", "move", Timestamp.valueOf("2023-06-09 12:05:00")),
       UserAction("3", "move", Timestamp.valueOf("2023-06-09 12:20:00")),
@@ -179,8 +212,8 @@ object UnmanagedStateEventTimeTimeout {
       UserAction("1", "move", Timestamp.valueOf("2023-06-09 13:10:00")),
       UserAction("3", "move", Timestamp.valueOf("2023-06-09 13:20:00")),
       UserAction("4", "move", Timestamp.valueOf("2023-06-09 13:30:00")),
-      // this last user will make 1, 3 and 4 drop, due to inactivity
-      UserAction("5", "move", Timestamp.valueOf("2023-06-09 14:30:00")),
+      // this last user will make 1, 3 and 4 drop due to inactivity
+      UserAction("5", "move", Timestamp.valueOf("2023-06-09 14:30:00"))
     )
 
     sampleData.foreach { record =>
